d7b5487a41df525f1495b3f64e0e245ff15fc339
AlexDanWalker <danteariza85@gmail.com>
2025-04-24 10:16:59 -0500

    Update Ejercicio 4.py

--- Daniel/Ejercicio 4.py
+++ Daniel/Ejercicio 4.py
@@ -1,10 +1,31 @@
 #Pide dos números e indica cuál es mayor o si son iguales.
-numero1 = int (input ("Escribe un numero =>"))
-numero2 = int (input ("Escribe otro numero =>"))
+numero1 = int(input("Escribe un numero =>"))
+numero2 = int(input("Escribe otro numero =>"))
 
 if numero1 > numero2:
-    print (f"{numero1} es mayor que {numero2}")
+    print(f"{numero1} es mayor que {numero2}")
 elif numero2 > numero1:
-    print (f"{numero2} es mayor que {numero1}")
+    print(f"{numero2} es mayor que {numero1}")
 else:
-    print ("Ambos numero son iguales")
+    print("Ambos numero son iguales")
+
+#Solicita una edad y determina si la persona es menor de edad (menor a 18).
+
+edad = int(input("Escriba su edad => "))
+
+if edad < 18 and edad > 0:
+    print("Eres menor de edad")
+elif edad >= 18:
+    print("Eres mayor de edad")
+else:
+    print("La edad ingresada es inválida")
+
+#Escribe un programa que compare dos cadenas de texto e indique si son iguales o distintas.
+
+cadena1 = input("Escribe la primera cadena de texto. => ")
+cadena2 = input("Escribe la segunda cadena de texto. => ") 
+
+if cadena1 == cadena2: 
+    print ("Las dos cadenas de texto son iguales.")
+else:
+    print ("Las dos cadenas de texto son diferentes.")
